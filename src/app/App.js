80070008d51fdce17d10f82c8a67cf15f5f32d8f
Christopher Edynak <christopher.m.a.edynak@gmail.com>
2019-05-08 18:20:34 -0400

    intialized and set state

--- src/app/App.js
+++ src/app/App.js
@@ -12,18 +12,33 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {products:[]};
     // Bind functions
     this.loadData = this.loadData.bind(this);
+    this.productList = this.productList.bind(this);
 
     this.loadData();
   }
 
   loadData = () => {
-    http.getProducts().then(products => {
-      console.log(products);
+    let self = this;
+    http.getProducts().then(data => {
+      self.setState({products: data})
     }, err => {
 
-    })
+    });
+  }
+
+  productList = () => {
+    const list = this.state.products.map((product) => 
+      <div className="col-sm-4" key={product._id}>
+        <Product title={product.title} price={product.price} imgUrl={product.imgUrl}/>
+      </div>
+  
+    );
+
+  return (list);
+
   }
 
   render(){
@@ -43,7 +58,7 @@ class App extends Component {
       </header>
       <div className="container App-main">
         <div className="row">
-        
+          {this.productList()}
         </div>
           <Product />
         </div>
